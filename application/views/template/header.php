4cd97e355e725bec601b938beaafa5dc89545581
Andrey Kudakov <kudakovandrey@gmail.com>
2017-09-10 21:25:33 +0300

    10.09.2017 21:25

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -15,6 +15,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $title;?></title>
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
     <link rel="stylesheet" href="assets/css/main_style.css">
 </head>
 <body>
--- application/views/template/main.php
+++ application/views/template/main.php
@@ -3,15 +3,18 @@
         <div>
             <img src="assets/img/logo-exphelper.png">
         </div>
-
     </div>
     <div id="main-menu" class="block">
 
     </div>
     <div class="block">
-        <div class="block-title"><?php echo $title;?></div>
+        <div class="block-title"><?php echo $block_title;?></div>
         <div class="block-content">
+            <form action="">
+                <select name="" id="">
 
+                </select>
+            </form>
         </div>
     </div>
 </div>
